825dabc1db1ceab3837d61b6400c064320cff90b
HassanAlizadeh <hassan.alizadeh529@gmail.com>
2021-03-16 02:10:53 +0330

    Add DateUtils to change format of datetime for showing

---
+++ app/src/main/java/com/android/shared/utils/DateUtils.kt
@@ -0,0 +1,35 @@
+package com.android.shared.utils
+
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+
+object DateUtils {
+
+    const val DATE_FORMAT_12 = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+    const val DATE_FORMAT_2 = "h:mm a"
+
+    /**
+     * Convert one date format string  to another date format string in android
+     *
+     * @param inputDateFormat Input SimpleDateFormat
+     * @param outputDateFormat Output SimpleDateFormat
+     * @param inputDate input Date String
+     * @throws ParseException
+     */
+    @Throws(ParseException::class)
+    fun formatDateFromDateString(
+        inputDateFormat: String,
+        outputDateFormat: String,
+        inputDate: String
+    ): String {
+        val mParsedDate: Date
+        val mOutputDateString: String
+        val mInputDateFormat = SimpleDateFormat(inputDateFormat, Locale.getDefault())
+        val mOutputDateFormat = SimpleDateFormat(outputDateFormat, Locale.getDefault())
+        mParsedDate = mInputDateFormat.parse(inputDate)
+        mOutputDateString = mOutputDateFormat.format(mParsedDate)
+        return mOutputDateString
+    }
+
+}
